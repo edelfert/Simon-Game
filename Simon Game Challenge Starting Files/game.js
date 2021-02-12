9345745e57cdecbf7e15742bd814746b632c4f4a
Eddie <github.towage@aleeas.com>
2021-02-12 15:00:24 -0800

    Completed steps 8-10. Added game functionality and ability to restart.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,22 +6,35 @@ var buttonColors = [
 ]
 
 let gamePattern = []
-
 let userClickedPattern = []
 
 let level = 0;
-
 var started = false
 //store previously pressed colors
 
 $(document).ready (function () {
   $('.btn').click(function() {
+    if (started === true) {
     var userChosenColor = $(this).attr('id');
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern)
     playSound(userChosenColor)
     animatePress(userChosenColor)
+    checkAnswer(userClickedPattern.length - 1)
+
+    }
+
   })
+  })
+
+// start game
+  $(document).keydown(function() {
+    if (started === false) {
+  nextSequence();
+  started = true
+  $('h1').text("Level 0")
+  }
+
   })
 
 //random number and animation
@@ -34,12 +47,15 @@ function nextSequence() {
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
 
-  level++;
   $("h1").text("Level " + level)
+  level++;
+
+
+  userClickedPattern = []
 }
 
 
-//sounds, animation, game start
+//sounds / animation
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
@@ -53,11 +69,33 @@ function animatePress(currentColor) {
   }, 100)
 }
 
-$(document).keydown(function() {
-  if (started === false) {
-nextSequence();
-started = true
-$('h1').text("Level 0")
-}
+//check answers
 
-})
+function checkAnswer(currentLevel) {
+if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+  console.log("win")
+  if (userClickedPattern.length === gamePattern.length) {
+
+    setTimeout (function () {
+    nextSequence()
+
+    }, 1000)
+  }
+} else {
+  console.log("failure")
+  playSound("wrong")
+  $('body').addClass("game-over")
+  setTimeout(function() {
+    $('body').removeClass("game-over")
+  }, 200)
+
+  $('h1').text("Game Over, Press Any Key to Restart")
+
+  startOver()
+}}
+
+function startOver () {
+  level = 0
+  gamePattern = []
+  started = false
+}
